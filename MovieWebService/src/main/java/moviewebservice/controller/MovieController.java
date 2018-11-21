d1daa25db0bc46d00d399fd5d8d52da3fa70031e
HerCerM <42811720+HerCerM@users.noreply.github.com>
2018-11-20 18:50:15 -0600

    Endpoint /movies/{id}/poster agregado. Permite ver el póster de una película.

--- MovieWebService/src/main/java/moviewebservice/controller/MovieController.java
+++ MovieWebService/src/main/java/moviewebservice/controller/MovieController.java
@@ -1,12 +1,15 @@
 package moviewebservice.controller;
 
-import moviewebservice.model.Genre;
 import moviewebservice.model.Movie;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import moviewebservice.service.GenreService;
-import moviewebservice.service.MovieGenreService;
 import moviewebservice.service.MovieService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,13 @@ public class MovieController {
         return movieService.getMovieById(id);
     }
 
+    @GetMapping(value = "/{id}/poster", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getPosterByMovieId(@PathVariable int id) throws IOException {
+        Movie movie = movieService.getMovieById(id);
+        InputStream in = new URL(movie.getPoster_path()).openStream();
+        return IOUtils.toByteArray(in);
+    }
+    
     @GetMapping
     public List<Movie> getAllMovies() {
         return movieService.getAllMovies();
