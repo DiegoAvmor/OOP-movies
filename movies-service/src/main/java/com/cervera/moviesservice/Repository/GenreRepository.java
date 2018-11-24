202eaea5fb146ffd5345a37290902dd65d30db7d
HerCerM <42811720+HerCerM@users.noreply.github.com>
2018-11-24 13:07:04 -0600

    Eliminación de proyecto demo

--- movies-service/src/main/java/com/cervera/moviesservice/Repository/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cervera.moviesservice.Repository;
-
-import com.cervera.moviesservice.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Integer> {
-
-}
--- movies-service/src/main/java/com/cervera/moviesservice/Repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cervera.moviesservice.Repository;
-
-import com.cervera.moviesservice.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
