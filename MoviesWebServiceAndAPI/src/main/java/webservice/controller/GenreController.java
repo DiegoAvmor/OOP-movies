3d6d1790b4ae79b1fba6afbcbfb73fa42816f090
DiegoAvmor <42280085+DiegoAvmor@users.noreply.github.com>
2018-12-06 00:07:12 -0600

    JavaDoc

--- MoviesWebServiceAndAPI/src/main/java/webservice/controller/GenreController.java
+++ MoviesWebServiceAndAPI/src/main/java/webservice/controller/GenreController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import webservice.model.Genre;
 import webservice.service.GenreService;
-
 import java.util.List;
+
+ /**
+ * Clase que maneja las acciones con respecto a los generos, continene un unico metodo
+ * el cual obtiene el listado completo de los generos de peliculas.
+ */
 @RestController
 @RequestMapping("genres")
 public class GenreController {
